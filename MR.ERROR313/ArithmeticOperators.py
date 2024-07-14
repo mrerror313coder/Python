4b5e5aae460d4acf200b9f0180f7e2a57bee8a48
MR. ERROR 313 <asadriazmerani@gmail.com>
2024-07-14 08:03:54 +0500

    Modification

--- MR.ERROR313/ArithmeticOperators.py
+++ MR.ERROR313/ArithmeticOperators.py
@@ -1,11 +1,16 @@
-a=10
-b=20
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a//b)
-print(a%b)
-print(a**b)
-print(a/b)
-print(b//3)
+# a=10
+# b=20
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+# print(a/b)
+# print(b//3)
+
+
+import numpy as np
+a = np.array([1, 2, 3])
+print(a)
